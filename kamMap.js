f8b09a70fbc90f5fa589b9046bea5f1369a91e36
nguyen842-ai <nguyen842@gmail.com>
2026-06-11 18:30:28 +0700

    Create kamMap.js

---
+++ kamMap.js
@@ -0,0 +1,12 @@
+// =========================================================
+// BẢNG TỪ ĐIỂN ĐỔI TÊN KAM CŨ THÀNH KAM MỚI
+// Hướng dẫn: Viết HOA toàn bộ tên cũ bên trái, tên mới bên phải
+// =========================================================
+
+const kamMap = {
+   "TRẦN ĐÌNH PHÚC": "PHẠM CHIẾN THẮNG",
+"PHẠM MINH SANG": "PHAN HỒ THANH TRÚC",
+"ĐOÀN VĂN TUẤN": "VACANT KAM HCM",
+"PHAN VĂN SO": "HUỲNH VĂN GIÀU",
+
+};
